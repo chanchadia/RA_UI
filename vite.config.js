62ddbfb53d9a7f61acd7b232a7702eb6338aceb7
Bhavesh Chanchadia <bhavesh.chanchadia@gmail.com>
2025-09-01 10:17:20 +0530

    init 1

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    esbuildOptions: {
+      sourcemap: false,
+    }
+  },
+  build: {
+    sourcemap: false, // Disables source map generation
+  },
 })
